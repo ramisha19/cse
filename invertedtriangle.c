1e6ee67f422eddc65a8a020ba0c28bae35bc2472
ramisha19 <ramisha.cse23.cu@gmail.com>
2024-06-04 08:06:59 +0600

    arrays

---
+++ invertedtriangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int i,k;
+
+   for(i=6;i>=1;i--){
+    for(k=1;k<=i;k++){
+        printf(" * ");
+        }
+        printf("\n");
+    
+   } 
+   return 0;
+}
